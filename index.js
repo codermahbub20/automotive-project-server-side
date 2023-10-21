e114a0b7a732e1a93a3fbcaab4f6f497887bc30f
codermahbub20 <mkrefat5@gmail.com>
2023-10-21 13:18:57 +0600

    Product update using put

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express()
@@ -34,35 +34,63 @@ async function run() {
 
 
 
-
-        app.post('/product',async(req,res) =>{
+        app.post('/product', async (req, res) => {
             const product = req.body;
             const result = await productCollection.insertOne(product)
             console.log(result)
             res.send(result)
         })
 
-        app.get('/product',async(req,res) =>{
+        app.get('/product', async (req, res) => {
             const cursor = productCollection.find()
             const result = await cursor.toArray();
             res.send(result)
         })
 
-        app.get('/product/:brand',async(req,res) =>{
-            const brand =  req.params.brand;
-            const query = {brand: brand}
+        app.get('/productByBrand/:brand', async (req, res) => {
+            const brand = req.params.brand;
+            const query = { brand: brand }
             const cursor = await productCollection.find(query).toArray();
             // const result = await cursor.toArray();
             res.send(cursor)
         })
 
 
+        // Update a product
 
+        app.get('/productById/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.send(result)
+        })
+
+        // Update product put
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true};
+            const updateProduct = req.body;
+            const product = {
+                $set: {
+                    name: updateProduct.name,
+                    brand: updateProduct.brand,
+                    type: updateProduct.type,
+                    price: updateProduct.price,
+                    description: updateProduct.description,
+                    rating: updateProduct.rating,
+                    photo: updateProduct.photo,
+
+                }
+            }
+            const result = await productCollection.updateOne(filter, product,options)
+            res.send(result)
+        })
 
 
 
         // User collection
-        app.post('/user', async(req, res) => {
+        app.post('/user', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user)
             console.log(result)
