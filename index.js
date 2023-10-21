ab7e1c0be1ac46994499470138ac708d1bbabe3d
codermahbub20 <mkrefat5@gmail.com>
2023-10-21 20:25:18 +0600

    Cart to product delte are implement

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ async function run() {
             res.send(result)
         })
 
+        // delete related code
+
+        app.delete('/cart/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
+
 // Product related code
         app.get('/product', async (req, res) => {
             const cursor = productCollection.find()
